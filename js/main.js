fc13c9254247440095436937322153c0a5bc98f5
Aleks <Allexa678@gmail.com>
2020-02-16 22:57:31 +0300

    Добавляет карточку объявления первого элемента

--- js/main.js
+++ js/main.js
@@ -18,6 +18,8 @@ var DESCRIPTIONS = ['Comfortable, bright and super-centrally located.', 'A relax
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
+var offerTypes = {palace: 'Дворец', flat: 'Квартира', house: 'Дом', bungalo: 'Бунгало'};
+var offerPhotos = {class: 'popup__photo', width: 45, height: 40, alt: 'Фотография жилья'};
 
 // определяет ширину блока
 var maxWidth = document.querySelector('.map').clientWidth;
@@ -27,13 +29,18 @@ var map = document.querySelector('.map');
 map.classList.remove('map--faded');
 
 // шаблон
-var template = document.querySelector('#pin');
+var templatePin = document.querySelector('#pin');
+var templateCard = document.querySelector('#card');
 
-// часть шаблона - маркер на карте Токио
-var mapPinTemplate = template.content.querySelector('.map__pin');
+// часть шаблона
+var mapPinTemplate = templatePin.content.querySelector('.map__pin');
+var mapCardTemplate = templateCard.content.querySelector('.map__card');
 
 // оъект DOM, содержащий список маркеров
 var pinsList = document.querySelector('.map__pins');
+
+// блок перед которым надо вставить карточку объявления
+var filtersContainer = map.querySelector('.map__filters-container');
 
 // функция возвращает случайное целое число от min(вкл) до max(вкл)
 var getRandomInteger = function (min, max) {
@@ -61,11 +68,11 @@ var getRandomAdvert = function () {
 
     adverts[i] = {
       author: {
-        avatar: 'img/avatars/user0' + (i + 1) + '.png',
+        avatar: 'img/avatars/user0' + (i + 1) + '.png'
       },
       offer: {
         title: getRandomElementArray(TITLES),
-        address: coordinate.x + ', ' + coordinate.y,
+        address: coordinate.x + ', ' + coordinate.y
       },
       price: getRandomInteger(MIN_PRICE, MAX_PRICE),
       type: getRandomElementArray(TYPES),
@@ -85,7 +92,7 @@ var getRandomAdvert = function () {
   return adverts;
 };
 
-// функция создает DOM-элемент, соответствующиЙ меткам на карте
+// 1. функция создает метки объявлений
 var createPinElement = function (pin) {
   var pinElement = mapPinTemplate.cloneNode(true);
   pinElement.style = 'left: ' + (pin.location.x - PIN_WIDTH / 2) + 'px; top: ' + (pin.location.y - PIN_HEIGHT) + 'px;';
@@ -94,14 +101,64 @@ var createPinElement = function (pin) {
   return pinElement;
 };
 
-// функция отрисовывает сгенерированные DOM-элементы
-var renderPin = function (pins) {
+// функция удаляет все дочерние элементы
+var removeChildElements = function (element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+};
+
+// функция создает новое удобство
+var makeFeatureElement = function (modifier) {
+  var newFeatureElement = document.createElement('li');
+  newFeatureElement.classList.add('popup__feature', 'popup__feature--' + modifier);
+  return newFeatureElement;
+};
+
+// функция создает новое фото
+var makePhotoElement = function (path) {
+  var newPhotoElement = document.createElement('img');
+  newPhotoElement.src = path;
+  newPhotoElement.classList.add(offerPhotos.class);
+  newPhotoElement.style.width = offerPhotos.width + 'px';
+  newPhotoElement.style.height = offerPhotos.height + 'px';
+  newPhotoElement.alt = offerPhotos.alt;
+  return newPhotoElement;
+};
+
+// 2. функция создает карточки объявлений
+var createCardElement = function (card) {
+  var cardElement = mapCardTemplate.cloneNode(true);
+  var photoParent = cardElement.querySelector('.popup__photos');
+  var featureParent = cardElement.querySelector('.popup__features');
+  cardElement.querySelector('.popup__title').textContent = card.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = card.offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = card.price + '₽/ночь';
+  cardElement.querySelector('.popup__type').textContent = offerTypes[card.type];
+  cardElement.querySelector('.popup__text--capacity').textContent = card.rooms + ' комнаты для ' + card.guests + ' гостей';
+  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + card.checkin + ', выезд до ' + card.checkout;
+  removeChildElements(featureParent);
+  card.features.forEach(function (item) {
+    featureParent.appendChild(makeFeatureElement(item));
+  });
+  cardElement.querySelector('.popup__description').textContent = card.description;
+  removeChildElements(photoParent);
+  card.photos.forEach(function (item) {
+    photoParent.appendChild(makePhotoElement(item));
+  });
+  cardElement.querySelector('.popup__avatar').src = card.author.avatar;
+  return cardElement;
+};
+
+// функция отрисовывает сгенерированные метки и карточку объявления на карте
+var renderElement = function (array) {
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < AMOUNT_ADVERT; i++) {
-    fragment.appendChild(createPinElement(pins[i]));
+    fragment.appendChild(createPinElement(array[i]));
   }
   pinsList.appendChild(fragment);
+  map.insertBefore(createCardElement(array[0]), filtersContainer);
 };
 
 var adverts = getRandomAdvert();
-renderPin(adverts);
+renderElement(adverts);
